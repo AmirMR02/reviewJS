627cf10cbaaccdd62d5680e1ff7d8a047f0c0d14
amirMR02 <a.m.rashvand02@gmail.com>
2024-03-14 11:03:35 +0330

    variables

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Hello world to js community");
+
+//values :
+//consider you are going to buy some fruit
+/**
+ * if you want to buy apple , banana ,orange
+ * you can't carry buy just two hands
+ * so you have to put it something like plastic
+ * and this is a simple called variable
+ * ->variable that carry a value
+ * -> a plastic that carry the fruit
+ */
+//three type of definition variable : const , var , let
+//const : constant length an can't change
+//let can chage after
+const apples = 12;
